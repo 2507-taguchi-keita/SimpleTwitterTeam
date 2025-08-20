bbfc6998c9c75272e2d33ad8e552100e3859a6f8
tanaka_eita <tanaka.eita@alhinc.jp>
2025-08-20 11:18:35 +0900

    コンフリクトのチェック用コミット

--- src/chapter6/dao/UserMessageDao.java
+++ src/chapter6/dao/UserMessageDao.java
@@ -9,12 +9,14 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
+
 import chapter6.beans.UserMessage;
 import chapter6.exception.SQLRuntimeException;
 
 public class UserMessageDao {
 
-	public List<UserMessage> select(Connection connection, Integer userId, String start, String end, int num) {
+	public List<UserMessage> select(Connection connection, Integer userId, String start, String end, String searchWord, String likeSearch, int num) {
 
         PreparedStatement ps = null;
         try {
@@ -33,6 +35,9 @@ public class UserMessageDao {
             if(userId != null) {
             	sql.append("AND user_id = ? ");
             }
+            if (!StringUtils.isBlank(searchWord)) {
+    			sql.append(" AND messages.text like ? ");
+    		}
             sql.append("ORDER BY created_date DESC limit " + num);
             ps = connection.prepareStatement(sql.toString());
 
@@ -41,7 +46,15 @@ public class UserMessageDao {
 
             if(userId != null) {
             	ps.setInt(3, userId);
-            }
+
+            	if (!StringUtils.isBlank(searchWord)) {
+    				ps.setString(4, "%" + searchWord + "%");
+    			}
+            } else {
+    			if (!StringUtils.isBlank(searchWord)) {
+    				ps.setString(3, "%" + searchWord + "%");
+    			}
+    		}
 
             ResultSet rs = ps.executeQuery();
 
